3cc4a5e5da04bdcd80638e575f45bd8433a85132
ryuseifujisaki <fujiryu1122@gmail.com>
2022-04-07 16:22:02 +0900

    first commit

---
+++ api/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/labstack/echo/v4"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func main() {
+	fmt.Println("hello world")
+}
