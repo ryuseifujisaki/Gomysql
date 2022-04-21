4933cfc3a5e34732190585f614da2ba60e3f89b9
ryuseifujisaki <fujiryu1122@gmail.com>
2022-04-21 14:01:44 +0900

    [FINISH] ディレクトリ構成

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,30 +1,30 @@
-package main
+// package main
 
-import (
-	"net/http"
-	_ "github.com/pkg/errors"
-	"github.com/labstack/echo"
-  "github.com/labstack/echo/middleware"
-	_ "github.com/go-sql-driver/mysql"
-)
+// import (
+// 	"net/http"
+// 	_ "github.com/pkg/errors"
+// 	"github.com/labstack/echo"
+//   "github.com/labstack/echo/middleware"
+// 	_ "github.com/go-sql-driver/mysql"
+// )
 
 
-func main() {
+// func main() {
 	
-	//インスタンスの作成
-	e := echo.New()
+// 	//インスタンスの作成
+// 	e := echo.New()
 
-	//ログ
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+// 	//ログ
+// 	e.Use(middleware.Logger())
+// 	e.Use(middleware.Recover())
 
-	//ルーティング
-	e.GET("/",hello)
+// 	//ルーティング
+// 	e.GET("/",hello)
 
-	e.Logger.Fatal(e.Start(":1323"))
-}
+// 	e.Logger.Fatal(e.Start(":1323"))
+// }
 
-//ハンドラーを定義
-func hello (c echo.Context) error {
-	return c.String(http.StatusOK, "Hello,World")
-}
+// //ハンドラーを定義
+// func hello (c echo.Context) error {
+// 	return c.String(http.StatusOK, "Hello,World")
+// }
