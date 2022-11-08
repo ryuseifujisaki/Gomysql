2fc4006625215dece6bed8c1c049dc47a56f1917
ryuseifujisaki <fujiryu1122@gmail.com>
2022-11-08 13:20:09 +0900

    go lint test2

--- api/src/domain/user.go
+++ api/src/domain/user.go
@@ -3,5 +3,8 @@ package domain
 type User struct {
     ID   int    `json:"id" gorm:"primary_key"`
     Name string `json:"name" gorm:""`
-    Age int `json:"age" gorm:""`
+    
+        Agis int `json:"age" gorm:""`
 }
+
+
