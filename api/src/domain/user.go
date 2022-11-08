fac2893066d16f119d994d4b1b34bf5cebc6b428
ryuseifujisaki <fujiryu1122@gmail.com>
2022-11-08 13:23:57 +0900

    github actions test

--- api/src/domain/user.go
+++ api/src/domain/user.go
@@ -3,7 +3,9 @@ package domain
 type User struct {
     ID   int    `json:"id" gorm:"primary_key"`
     Name string `json:"name" gorm:""`
-      Agis int `json:"age" gorm:""`
+    Age int `json:"age" gorm:""`
 }
+
+fmt.Println(veo)
 
 
