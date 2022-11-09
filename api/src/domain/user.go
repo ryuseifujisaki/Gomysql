0a1a40f11da1536ea06038f30889a0039d3c4834
ryuseifujisaki <fujiryu1122@gmail.com>
2022-11-09 16:09:58 +0900

    lint.yml修正

--- api/src/domain/user.go
+++ api/src/domain/user.go
@@ -3,7 +3,7 @@ package domain
 type User struct {
     ID   int    `json:"id" gorm:"primary_key"`
     Name string `json:"name" gorm:""`
-    　Age int `json:"age" gorm:""`
+    Age int `json:"age" gorm:""`
 }
 
 
