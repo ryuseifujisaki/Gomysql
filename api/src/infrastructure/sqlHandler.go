0adf38d470cf76cd5248aeeda485297c972a0fcf
ryuseifujisaki <fujiryu1122@gmail.com>
2022-11-09 23:50:38 +0900

    lint test

--- api/src/infrastructure/sqlHandler.go
+++ api/src/infrastructure/sqlHandler.go
@@ -35,4 +35,6 @@ func (handler *SqlHandler) FindAll(obj interface{}) {
 func (handler *SqlHandler) DeleteById(obj interface{}, id string) {
 	handler.db.Delete(obj, id)
 	 fmt.Println( "asas" )
+	 fmt.Println( "asas" )
+
 }
